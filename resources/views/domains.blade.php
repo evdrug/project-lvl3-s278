8423125f211f31d49f4b31678ffaede98b84a0fd
evdrug <druzyakin.e@gmail.com>
2018-07-17 00:45:17 +0300

    connect DB

--- resources/views/domains.blade.php
+++ resources/views/domains.blade.php
@@ -4,5 +4,10 @@
     List Domains
 @endsection
 @section('content')
-    {{print_r($domains)}}
+    @foreach($domains as $domain)
+        <p>This is domain {{ $domain->id }}</p>
+        <p>This is domain {{ $domain->name }}</p>
+        <p>This is domain {{ $domain->created_at }}</p>
+        <p>This is domain {{ $domain->updated_at }}</p>
+    @endforeach
 @endsection
